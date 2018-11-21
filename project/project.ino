6ac550a56e701b93934ad68f74fbd2db808c5f5a
karthikmohan14 <karthikmkochi@gmail.com>
2018-11-21 12:32:45 +0530

    fixed seizure

--- project/project.ino
+++ project/project.ino
@@ -5,7 +5,7 @@
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
 double roll = 0.00, pitch = 0.00, x = 0.00, y = 0.00, z = 0.00;
-double mini = 0.00, maxi = 0.00, xf = 0.00, yf = 0.00, zf = 0.00, xd = 0.00, yd = 0.00, zd = 0.00;
+double mini = 0.00, fmini = 0.10, maxi = 0.00, xf = 0.00, yf = 0.00, zf = 0.00, xd = 0.00, yd = 0.00, zd = 0.00;
 int timecounter = 0;
 
 void setup() {
@@ -32,25 +32,46 @@ void checkMovement() {
  }
 }
 
-
-void checkFits(){
-  if ((timecounter % 250) == 0) {
+int i = 0;
+int flag[5] = {0,0,0,0,0};
+void checkFits()
+{
+  Serial.print("checking for seizures : ");
+  Serial.print(flag[0]);Serial.print(",");
+  Serial.print(flag[1]);Serial.print(",");
+  Serial.print(flag[2]);Serial.print(",");
+  Serial.print(flag[3]);Serial.print(",");
+  Serial.print(flag[4]);Serial.print("");
+  i++;
+  if(i==5){i=0;}
+  if ((timecounter % 1) == 0) {
    xd = xf - x;
    yd = yf - y;
    zd = zf - z;
-   Serial.print("F");
-   Serial.print(",");
-   checkMovement();
-  }
+   if(xd < 0){xd *= -1;}
+   if(yd < 0){yd *= -1;}
+   if(zd < 0){zd *= -1;}
+  if(xd >  fmini && yd >  fmini && zd >  fmini){
+    flag[i] = 1;
+    }else{
+      flag[i] = 0;
+      }
+  if(flag[0] == 1 && flag[1] == 1 && flag[2] == 1 && flag[3] == 1 && flag[4] == 1){
+  Serial.print("you have seizures !!");  
+  }  
 }
-
-void cascadeMovement() {
+}
+void loop() {
   timecounter++;
   Serial.print(timecounter);
   Serial.print(",");
-  if (timecounter > 10000) {
-   timecounter = 0;
-  } else if ((timecounter % 250) == 0) {
+  if (timecounter > 1250) {
+     timecounter = 0;
+  }
+  else if (timecounter > 1000){
+    checkFits();
+  }
+  else if ((timecounter % 50) == 0) {
    xd = xf - x;
    yd = yf - y;
    zd = zf - z;
@@ -59,24 +80,29 @@ void cascadeMovement() {
    z = zf;
    Serial.print("!");
    Serial.print(",");
-   checkMovement();
+   if (xd > yd && xd > zd && xd > mini) {
+  Serial.print("x rules");
+  Serial.print(",");
+ }
+ if (yd > xd && yd > zd && yd > mini) {
+  Serial.print("y rules");
+  Serial.print(",");
+ }
+ if (zd > yd && zd > xd && zd > mini) {
+  Serial.print("z rules");
+  Serial.print(",");
+ }
   }
-}  
-  void moveAccelerometer() {
-   sensors_event_t event;
+sensors_event_t event;
    accel.getEvent( & event);
    xf = (event.acceleration.x) / 9.8;
-   Serial.print(xf);
+   Serial.print(xd);
    Serial.print(",");
    yf = (event.acceleration.y) / 9.8;
-   Serial.print(yf);
+   Serial.print(yd);
    Serial.print(",");
    zf = (event.acceleration.z) / 9.8;
-   Serial.print(zf);
+   Serial.print(zd);
    Serial.println("");
    delay(10);
-  }
-void loop() {
- cascadeMovement();
- moveAccelerometer();
 }
